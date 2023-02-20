2fa515dd1a775aa4d19829e53aab509495678047
TrebbleBiscuit <TrebbleBiscuit@users.noreply.github.com>
2023-02-19 19:26:34 -0500

    trying to make it cleaner

--- rust/main.rs
+++ rust/main.rs
@@ -3,22 +3,15 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    // human_vs_ai_rps_loop()
-    ai_vs_ai_rps_loop()
+    play_rps_forever(Strats::Human, Strats::Zhejiang)
 }
 
 enum Strats {
+    Human,
     Zhejiang,
     Random,
 }
 // todo: specify ome of these in arg?
-
-enum RPSOptions {
-    Rock,
-    Paper,
-    Scissors,
-}
-// todo: use these instead of i32
 
 struct RPSGame {
     total_ties: i32,
@@ -27,19 +20,22 @@ struct RPSGame {
     last_p1_guess: i32,
     last_p2_guess: i32,
     last_result: i32,
+    p1_strat: Strats,
+    p2_strat: Strats,
 }
 
 impl RPSGame {
-    fn rps_round(&mut self, human_guess: i32) -> i32 {
-        // return 1 for victory, 0 for tie, -1 for defeat
-        let ai_guess = self.get_ai_guess(self.last_p1_guess, self.last_result, 50);
+    fn rps_round(&mut self) -> i32 {
+        // return 1 for player 1 victory, 0 for tie, -1 for defeat
+        let p1_guess = self.guess_from_strat(&self.p1_strat, self.last_p2_guess, -self.last_result);
+        let p2_guess = self.guess_from_strat(&self.p2_strat, self.last_p1_guess, self.last_result);
         // let ai_guess_name = choice_name(ai_guess);
         // let guess_name = choice_name(human_guess);
         // println!("AI chose {ai_guess_name}");
         // println!("You chose {guess_name}");
 
         let result: i32;
-        match human_guess - ai_guess {
+        match p1_guess - p2_guess {
             0 => result = 0,
             -2 => result = 1,
             -1 => result = -1,
@@ -47,25 +43,35 @@ impl RPSGame {
             2 => result = -1,
             _ => panic!("Invalid guess comparison"),
         }
+        match result {
+            1 => self.total_wins += 1,
+            0 => self.total_ties += 1,
+            -1 => self.total_losses += 1,
+            _ => panic!("RPS returned invalid result"),
+        }
         self.last_result = result;
-        self.last_p1_guess = human_guess;
+        self.last_p1_guess = p1_guess;
+        self.last_p2_guess = p2_guess;
         return result;
     }
 
-    fn get_ai_guess(&self, last_opp_guess: i32, last_result: i32, random_chance: i32) -> i32 {
-        // last_opp_guess is the opposing player's last guess
-        // last result is relative to the opponent i.e. 1 means opponent won
-        // random_chance is between 0 and 100
-        // zhejiang strat 1/3 the time, random 2/3
-        if rand::thread_rng().gen_range(1..=100) <= random_chance {
-            zhejiang(last_opp_guess, last_result)
-        } else {
-            rand::thread_rng().gen_range(1..=3)
+    fn guess_from_strat(&self, strat: &Strats, last_opp_guess: i32, last_result: i32) -> i32 {
+        match strat {
+            Strats::Zhejiang => {
+                // add some randomness to zhejiang so it's not so predictable
+                if rand::thread_rng().gen_range(1..=100) <= 30 {
+                    zhejiang(last_opp_guess, last_result)
+                } else {
+                    rand::thread_rng().gen_range(1..=3)
+                }
+            }
+            Strats::Human => solicit_human_guess(),
+            Strats::Random => rand::thread_rng().gen_range(1..=3),
         }
     }
 }
 
-fn make_rps_game() -> RPSGame {
+fn make_rps_game(p1_strat: Strats, p2_strat: Strats) -> RPSGame {
     RPSGame {
         total_ties: 0,
         total_losses: 0,
@@ -73,48 +79,22 @@ fn make_rps_game() -> RPSGame {
         last_p1_guess: rand::thread_rng().gen_range(1..=3),
         last_p2_guess: rand::thread_rng().gen_range(1..=3),
         last_result: 0,
+        p1_strat,
+        p2_strat,
     }
 }
 
-fn human_vs_ai_rps_loop() {
+fn play_rps_forever(p1_strat: Strats, p2_strat: Strats) {
     println!("Let's play rock paper scissors!");
-    let mut game = make_rps_game();
+    let mut game = make_rps_game(p1_strat, p2_strat);
     loop {
-        let human_guess = solicit_human_guess();
+        let result = game.rps_round();
         println!("AI chose {}", choice_name(game.last_p2_guess));
         println!("You chose {}", choice_name(game.last_p1_guess));
-        match game.rps_round(human_guess) {
-            1 => {
-                println!("You Win!");
-                game.total_wins += 1
-            }
-            0 => {
-                println!("It's a Tie!");
-                game.total_ties += 1
-            }
-            -1 => {
-                println!("You Lost!");
-                game.total_losses += 1
-            }
-            _ => panic!("RPS returned invalid result"),
-        }
-        println!(
-            "Record: {}-{}-{}",
-            game.total_wins, game.total_losses, game.total_ties
-        );
-        println!()
-    }
-}
-
-fn ai_vs_ai_rps_loop() {
-    println!("Let's play rock paper scissors!");
-    let mut game = make_rps_game();
-    loop {
-        let ai1_guess = game.get_ai_guess(game.last_p2_guess, -game.last_result, 10);
-        match game.rps_round(ai1_guess) {
-            1 => game.total_wins += 1,
-            0 => game.total_ties += 1,
-            -1 => game.total_losses += 1,
+        match result {
+            1 => println!("Player 1 wins!"),
+            0 => println!("It's a tie!"),
+            -1 => println!("Player 2 wins!"),
             _ => panic!("RPS returned invalid result"),
         }
         println!(
